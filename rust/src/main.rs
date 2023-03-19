7ba9b46da4d5bbbe64af3e6891cb2a00527e74f9
Marius Ihring <marius.ihring@icloud.com>
2023-03-19 11:02:31 +0100

    added alias start commands for dev with hot reloading and basic test route

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().service(test))
+        .bind(("127.0.0.1", 8081))?
+        .run()
+        .await
+}
+
+#[get("/test")]
+async fn test() -> HttpResponse {
+    HttpResponse::Ok().body("oK")
 }
