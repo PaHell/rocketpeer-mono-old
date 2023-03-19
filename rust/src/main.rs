4f39138896808fb162ec6ac9d8f21fc4154a6291
Marius Ihring <marius.ihring@icloud.com>
2023-03-19 12:13:43 +0100

    moved to rocket for swagger

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,12 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+#[macro_use]
+extern crate rocket;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().service(test))
-        .bind(("127.0.0.1", 8081))?
-        .run()
-        .await
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-#[get("/test")]
-async fn test() -> HttpResponse {
-    HttpResponse::Ok().body("oK")
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
