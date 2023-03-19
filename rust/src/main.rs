24682b198f243160470981ed6a3bd5fa5cd84e51
Marius Ihring <marius.ihring@icloud.com>
2023-03-19 13:51:31 +0100

    after years of fighting there is finally swagger working

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,30 @@
-#[macro_use]
-extern crate rocket;
+use rocket::form::FromForm;
+use rocket::{get, post, serde::json::Json};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[openapi]
+#[get("/example")]
+fn example_email() -> &'static str {
+    "test@example.com"
 }
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() {
+    let launch_result = rocket::build()
+        .mount("/", openapi_get_routes![example_email])
+        .mount(
+            "/swagger/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
