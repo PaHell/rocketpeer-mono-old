3899cf6a2e60ea31a268616e209860e617243827
Marius Ihring <marius.ihring@icloud.com>
2023-03-27 21:21:16 +0200

    working parser wowe

--- schema_parser/src/main.rs
+++ schema_parser/src/main.rs
@@ -19,6 +19,12 @@ struct Property {
     data_type: String,
 }
 
+#[derive(Debug)]
+struct Enum {
+    name: String,
+    entries: Vec<String>,
+}
+
 fn parse_model(input: &str) -> IResult<&str, Model> {
     let (input, (_, name, _, properties)) = tuple((
         tag("model"),
@@ -95,35 +101,80 @@ fn filter_properties(properties: &Vec<Property>) -> Vec<Property> {
         .collect()
 }
 
-fn generate_ts_interfaces(models: &Vec<Model>) -> String {
+fn generate_ts_interfaces(models: &Vec<Model>, enums: &Vec<Enum>) -> String {
     let mut output = String::new();
 
     for model in models {
-        output.push_str(&format!("interface{}{{\n", model.name));
+        output.push_str(&format!("export interface{}{{\n", model.name));
         for (i, property) in filter_properties(&model.properties).iter().enumerate() {
-            output.push_str(&format!("    {}: {};", property.name, property.data_type));
-            if i < model.properties.len() - 1 {
-                output.push_str("\n");
+            if property.name.contains("@@") {
+                continue;
+            }
+            if property.data_type.contains("?") {
+                let new_type: String = property.data_type.chars().filter(|&c| c != '?').collect();
+
+                output.push_str(&format!("    {}: {};\n", property.name, new_type));
             } else {
-                output.push_str("\n}\n\n");
+                output.push_str(&format!("    {}: {};\n", property.name, property.data_type));
             }
         }
+        output.push_str("\n}\n\n");
+    }
+    for x in enums {
+        output.push_str(&format!("export enum{}{{\n", x.name));
+        for entry in &x.entries {
+            output.push_str(&format!("    {},\n", &entry))
+        }
+
+        output.push_str("}\n")
     }
     output
 }
+
+fn parse_enum(input: &str) -> IResult<&str, Enum> {
+    let (input, (_, name, _, properties)) = tuple((
+        tag("enum"),
+        take_until("{"),
+        tag("{"),
+        many0(parse_enum_entries),
+    ))(input)?;
+
+    let output = Enum {
+        name: name.to_owned(),
+        entries: properties,
+    };
+    Ok((input, output))
+}
+
+fn parse_enum_entries(input: &str) -> IResult<&str, String> {
+    let (input, (_, mut entry_data, _)) = tuple((
+        take_while(char::is_whitespace),           // _
+        take_while1(|c: char| !c.is_whitespace()), // name
+        complete(take_until("\n")),                // @ stuff
+    ))(input)?;
+    if entry_data == "}" {
+        entry_data = "";
+    }
+
+    Ok((input, entry_data.to_owned()))
+}
 fn main() {
-    let input = fs::read_to_string("schema.prisma").expect("Failed to read schema");
+    let input = fs::read_to_string("rust/prisma/schema.prisma").expect("Failed to read schema");
     let mut data: Vec<String> = input.split("}\n\n").map(|s| s.to_string()).collect();
     data.drain(0..2);
     let mut models: Vec<Model> = Vec::new();
-    for mut model in data {
-        //let mut tester: String = model.lines().filter(|line| !line.contains("@@")).collect();
-        //tester.push_str("}");
-        let (_, output) = parse_model(&model).expect("failed to parse schema");
-        models.push(output);
+    let mut enums: Vec<Enum> = Vec::new();
+    for model in data {
+        if model.contains("model") {
+            let (_, output) = parse_model(&model).expect("failed to parse schema");
+            models.push(output);
+        } else if model.contains("enum") {
+            let (_, my_enum) = parse_enum(&model).expect("failed to parse enum");
+            enums.push(my_enum);
+        }
     }
 
-    let output = generate_ts_interfaces(&models);
+    let mut output = generate_ts_interfaces(&models, &enums);
 
-    fs::write("schema.d.ts", output).expect("failed to write ts file");
+    fs::write("sveltekit/src/database.d.ts", output).expect("failed to write ts file");
 }
