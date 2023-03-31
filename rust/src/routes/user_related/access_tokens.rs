14b5f83f27305802c232c24620e57d55e4f76622
Marius <marius.ihring@icloud.com>
2023-03-31 15:44:31 +0200

    user routes

--- rust/src/routes/user_related/access_tokens.rs
+++ rust/src/routes/user_related/access_tokens.rs
@@ -1,1 +1,8 @@
+use crate::prisma::PrismaClient;
+use actix_web::{web, HttpResponse};
 
+fn create_jwt() {}
+
+pub async fn get_token(client: web::Data<PrismaClient>) -> HttpResponse {
+    HttpResponse::Ok().body("Ok")
+}
